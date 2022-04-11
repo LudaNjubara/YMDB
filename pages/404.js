ae7a64c044e0d9cc4256a72c03c3cb902ce9b70c
Mihael Sestak <mihael.sestak00@gmail.com>
2022-04-11 05:53:13 +0200

    Added 404 page styling

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/404/page_404.module.css";
+
 function custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div className={styles.unavailableWrapper}>
+      <p className={styles.pageErrorText}>
+        <span className={styles.emptinessText}>Such emptiness...</span> Try finding your way back over
+        <Link href="/">
+          <a className={styles.homeLink}>here</a>
+        </Link>
+      </p>
+      <div className={styles.pageErrorImageContainer}>
+        <Image
+          src="/_404_.svg"
+          width={700}
+          height={600}
+          className={styles.pageErrorImage}
+          priority={true}
+          alt="Movie not found"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default custom404;
